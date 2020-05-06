258b83063b0cd1c2bb3f6a4c181b6b0e37ce9a3e
Marcelo Tokarnia <marcelo.tokarnia@klarna.com>
2020-05-07 00:01:18 +0200

    insertTime cli logic

--- cli/src/commands/insertTime.js
+++ cli/src/commands/insertTime.js
@@ -1,11 +1,34 @@
-import xml2js from 'xml2js'
 import fs from 'fs'
+import { last } from 'ramda'
+import xml2js from 'xml2js'
 
 export default ({ s, e, file }) => {
-  var parser = new xml2js.Parser()
+  const parser = new xml2js.Parser()
+  const builder = new xml2js.Builder()
+  const filename = last(file.split('/'))
+
   fs.readFile(file, function (err, data) {
+    if (err) {
+      throw err
+    }
+    const startTime = new Date(s)
+    const endTime = new Date(e)
     parser.parseString(data, function (err, result) {
-      console.dir(result.gpx.trk[0].trkseg[0].trkpt.length)
+      if (err) {
+        throw err
+      }
+      const trkpts = result.gpx.trk[0].trkseg[0].trkpt
+      const splits = (endTime - startTime) / (trkpts.length - 1)
+      result.gpx.metadata[0].time = [startTime.toISOString()]
+      trkpts.forEach((trkpt, i) => {
+        trkpt.time = [new Date(+startTime + i * splits).toISOString()]
+      })
+      const processed = builder.buildObject(result)
+      fs.writeFile(`output/insertTime-${filename}`, processed, err => {
+        if (err) {
+          throw err
+        }
+      })
     })
   })
 }
