bb6e63c1beb0c56434b5c57c38f8245d811ee19d
Marcelo Tokarnia <marcelo.tokarnia@klarna.com>
2020-06-18 22:10:53 +0200

    fix unauthenticated access to maps

--- src/graphql/resolvers/query/getStravaProfile.ts
+++ src/graphql/resolvers/query/getStravaProfile.ts
@@ -4,10 +4,12 @@ import { strava } from '../../../clients'
 import { sudoRefreshToken } from '../../../utils/manageTokens'
 
 export default async (_parent: any, args: any, context: any) => {
-  let access_token = context.req.cookies.access_token
+  let access_token
   if (args.mapId) {
     const { username } = await context.req.redis.get(KEYS.SAVED_MAP(args.mapId))
     access_token = await sudoRefreshToken(context.req, username)
+  } else {
+    access_token = context.req.cookies.access_token
   }
 
   return parseStravaProfile(await strava.getProfile(access_token))
