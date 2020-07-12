64bf10a71b7b2129379198badddf718090db7a9a
Marcelo Tokarnia <marcelo.tokarnia@klarna.com>
2020-07-12 12:51:59 +0200

    allow cors headers in backend

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const FRONTEND_HOST =
+  process.env.NODE_ENV === 'production' ? 'https://strava.tokks.tech' : 'http://localhost:3000'
--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,22 @@
 import * as Sentry from '@sentry/node'
 import bodyParser from 'body-parser'
-import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import express from 'express'
 import graphqlServer from './graphql'
 import mapRouter from './map/router'
 import redisMiddleware from './redisMiddleware'
 import stravaRouter from './strava/router'
+import { FRONTEND_HOST } from './constants'
 
 export const port = process.env.PORT || 8080
 
-const ALLOWED_CORS_ORIGINS = [
-  'https://strava-maps.herokuapp.com',
-  'https://strava.tokks.tech',
-  'http://localhost:3000',
-  'http://localhost:8080',
-]
-
 export default fn => {
   const app = express()
-  app.use(
-    cors({
-      credentials: true,
-      origin: function (origin, callback) {
-        // allow requests with no origin
-        // (like mobile apps or curl requests)
-        const CORS_ERROR = `The CORS policy for this site does not allow access from the specified Origin.`
-        const params: [Error | null, boolean] =
-          !origin || ALLOWED_CORS_ORIGINS.includes(origin)
-            ? [null, true]
-            : [new Error(CORS_ERROR), false]
-        return callback(...params)
-      },
-    })
-  )
+  app.use((_, res, next) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.setHeader('Access-Control-Allow-Origin', FRONTEND_HOST)
+    next()
+  })
   Sentry.init({ dsn: 'https://cd09b20da70241fea1782b8bbb6e15b4@o401355.ingest.sentry.io/5260828' })
   app.use(Sentry.Handlers.requestHandler())
 
@@ -42,7 +25,11 @@ export default fn => {
   app.use(redisMiddleware)
   app.use('/strava', stravaRouter)
   app.use('/map', mapRouter)
-  graphqlServer.applyMiddleware({ app, path: '/graphql' })
+  graphqlServer.applyMiddleware({
+    app,
+    path: '/graphql',
+    cors: { origin: FRONTEND_HOST, credentials: true },
+  })
 
   app.use(Sentry.Handlers.errorHandler())
 
