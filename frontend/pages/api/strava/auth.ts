99bde9695dc97b89fd37855ef7f8fb4942a8ecc6
Marcelo Tokarnia <marcelo.tokarnia@klarna.com>
2020-07-16 23:52:49 +0200

    remove console.log

--- frontend/pages/api/strava/auth.ts
+++ frontend/pages/api/strava/auth.ts
@@ -5,7 +5,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const remoteResponse = await forwardToRemote(req, res)
   switch (req.method) {
     case 'GET':
-      console.log(remoteResponse.data.redirectUri)
       return res
         .writeHead(307, {
           Location: remoteResponse.data.redirectUri,
