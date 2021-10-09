65dcde940e7419f44aeb7c9b2f65728f1db92a4b
Marcelo Tokarnia <marcelo.tokarnia@gmail.com>
2021-10-08 21:06:18 -0300

    add athlete name

--- src/graphql/resolvers/query/getStravaClubActivities.ts
+++ src/graphql/resolvers/query/getStravaClubActivities.ts
@@ -1,19 +1,25 @@
+import { assoc } from 'ramda'
 import { IFieldResolver } from 'apollo-server-express'
 import { MapsRequest } from '@src/interfaces/routes'
 import { ParsedActivity } from '@tokks/strava-parsed/typings'
 import parseStravaActivity from '@tokks/strava-parsed/parseActivity'
 import { strava } from '@src/clients'
-// import { sudoRefreshToken } from '@src/utils/manageTokens'
+import { sudoRefreshToken } from '@src/utils/manageTokens'
 
 const getStravaClubActivities: IFieldResolver<any, { req: MapsRequest }, { id: string }> = async (
   _parent: any,
   args: any,
   context: any
 ): Promise<Array<ParsedActivity>> => {
-  // const access_token = await sudoRefreshToken(context.req, 'mtokarnia')
-  const access_token = '39e6686877c4585d01452c5f0cb7a80825379f62'
+  const access_token = await sudoRefreshToken(context.req, 'mtokarnia')
 
-  return (await strava.getClubActivities(access_token, args.id)).map(parseStravaActivity)
+  return (await strava.getClubActivities(access_token, args.id)).map(e =>
+    assoc(
+      'athleteName',
+      `${(e.athlete as any).firstname} ${(e.athlete as any).lastname}`,
+      parseStravaActivity(e)
+    )
+  )
 }
 
 export default getStravaClubActivities
